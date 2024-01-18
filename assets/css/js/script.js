110b2673abd60f140f4b4426afe822b0714b2453
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-01-18 14:56:10 -0700

    made folder for two js pages

---
+++ assets/css/js/script.js
@@ -0,0 +1,54 @@
+// List of all questions
+
+var questions =  [
+
+{
+    prompt: "Who invented the corset?",
+    options: ["<Hedley Fishmane>", "<Gytha Weaver>", "<Roxey Ann Caplin>", "<Chancey Crawforde>"],
+    answer:"<Rocey Ann Caplin>"
+},
+
+{
+    prompt: "What year did men start wearing corsets?",
+    options: ["<1820>", "<1750>", "<1580>", "<1066>"],
+    answer:"<1820>"
+
+},
+
+{
+    prompt: "What was the most popular fashion accessory in the Victorian Era?",
+    options: ["<Taxidermy Hats>", "<Dresses with insects>", "<Accessories made out of hair>", "<All of the Above>"],
+    answer: "<All of the Above>"
+},
+
+{
+    prompt: "What made Elizabeth I's face white?", 
+    options: ["<Lead>", "<Mercury>","<Eggs Shells>","<Alum>"],
+    answer: "<Lead>"
+},
+
+{
+    prompt: "Who was the most popular disgners of the Victorian Era?",
+    options: ["<Lady Duff Gordon>", "<Jacques Doucet>", "<Jeanne Paquin and Worth>", "<All of the Above>"],
+    answer: "<All of the Above"
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
