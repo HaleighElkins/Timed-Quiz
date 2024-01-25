8461d3b8af933d2baea59b8091c8ab1ccc9ba7e3
HaleighElkins <152942336+HaleighElkins@users.noreply.github.com>
2024-01-24 17:50:36 -0700

    fixed bug to hide landing page and ending page. Also fixed the ending page to show up.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,38 @@
 // List of all questions
 
-var questions =  [
+var questions = [
 
     {
         prompt: "Who invented the corset?",
         options: ["<Hedley Fishmane>", "<Gytha Weaver>", "<Roxey Ann Caplin>", "<Chancey Crawforde>"],
-        answer:"<Roxey Ann Caplin>"
+        answer: "<Roxey Ann Caplin>"
     },
-    
+
     {
         prompt: "What year did men start wearing corsets?",
         options: ["<1820>", "<1750>", "<1580>", "<1066>"],
-        answer:"<1820>"
-    
+        answer: "<1820>"
+
     },
-    
+
     {
         prompt: "What was the most popular fashion accessory in the Victorian Era?",
         options: ["<Taxidermy Hats>", "<Dresses with insects>", "<Accessories made out of hair>", "<All of the Above>"],
         answer: "<All of the Above>"
     },
-    
+
     {
-        prompt: "What made Elizabeth I's face white?", 
-        options: ["<Lead>", "<Mercury>","<Eggs Shells>","<Alum>"],
+        prompt: "What made Elizabeth I's face white?",
+        options: ["<Lead>", "<Mercury>", "<Eggs Shells>", "<Alum>"],
         answer: "<Lead>"
     },
-    
+
     {
         prompt: "Who was the most popular disgners of the Victorian Era?",
         options: ["<Lady Duff Gordon>", "<Jacques Doucet>", "<Jeanne Paquin and Worth>", "<All of the Above>"],
         answer: "<All of the Above>"
     }];
-     
+
 
 
 // Dom Elements
@@ -45,9 +45,12 @@ var startBtn = document.querySelector("#start");
 var submitBtn = document.querySelector("#submit-score");
 var reStartBtn = document.querySelector("#restart");
 var feedbackEl = document.querySelector("#feedback");
+var landingScreenEl = document.getElementById("start-screen");
+var endScreenEl = document.getElementById("quiz-end");
+var finalScoreEl = document.getElementById("score-final");
 
 
-// Quiz Start and to hide the front page
+// Question index and timer 
 
 var currentQuestionIndex = 0;
 var time = questions.length * 15;
@@ -66,12 +69,14 @@ function clockTick() {
     // console.log("Clock Tick Check")
 }
 
+// Quiz Start Timer
+
 function quizStart() {
     timerId = setInterval(clockTick, 1000);
-    timerEl.textContent = time;
-    var landingScreenEl = document.getElementById("start-screen");
+    timerEl.textContent = time
     landingScreenEl.setAttribute("class", "hide");
     questionsEl.removeAttribute("class");
+    console.log("hiding landing page")
     getQuestions();
 }
 
@@ -81,14 +86,15 @@ startBtn.onclick = quizStart;
 
 
 
-// Loop for Questions
+// Loop for Questions and hiding landing page
 
 function getQuestions() {
+    var landingScreenEl = document.getElementById("start-screen").style.display = 'none'
+    endScreenEl.style.display = 'none'
     if (currentQuestionIndex > 4) {
         console.log("Game Ending")
         quizEnd()
     } else {
-
         var currentQuestion = questions[currentQuestionIndex];
         var promtEl = document.getElementById("questions-words");
         promtEl.textContent = currentQuestion.prompt;
@@ -112,37 +118,39 @@ function questionClick() {
         feedbackEl.textContent = `Wrong! The correct answer was ${questions[currentQuestionIndex].answer}.`;
         feedbackEl.style.color = "red";
         time -= 10;
-      if (time < 0) {
-        time = 0;
-      }
+        if (time < 0) {
+            time = 0;
+        }
     } else {
-      feedbackEl.textContent = "Correct!";
-      feedbackEl.style.color = "green";
+        feedbackEl.textContent = "Correct!";
+        feedbackEl.style.color = "green";
     }
     feedbackEl.setAttribute("class", "feedback");
-    setTimeout(function() {
-      feedbackEl.setAttribute("class", "feedback hide");
+    setTimeout(function () {
+        feedbackEl.setAttribute("class", "feedback hide");
     }, 2000);
     currentQuestionIndex++;
     if (currentQuestionIndex === questions.length) {
-      quizEnd();
+        quizEnd();
     } else {
-      getQuestions();
+        getQuestions();
     }
 }
 
 
 // Quiz End
 
-// Quiz Ending when user answers all questions
+// Quiz Ending when user answers all questions and the questions disapear
 
 function quizEnd() {
+    finalScoreEl.style.display = 'block'
+    endScreenEl.style.display = 'block'
     clearInterval(timerId);
-    var endScreenEl = document.getElementById("quiz-end");
     endScreenEl.removeAttribute("class");
-    var finalScoreEl = document.getElementById("score-final");
     finalScoreEl.textContent = time;
     questionsEl.setAttribute("class", "hide");
+    questionsEl.style.display = 'none'
+  
 }
 
 
@@ -151,14 +159,14 @@ function quizEnd() {
 function saveHighscore() {
     var name = nameEl.value.trim();
     if (name !== "") {
-      var highscores =
-        JSON.parse(window.localStorage.getItem("highscores")) || [];
-      var newScore = {
-        score: time,
-        name: name
-      };
-      highscores.push(newScore);
-      window.localStorage.setItem("highscores", JSON.stringify(highscores));
+        var highscores =
+            JSON.parse(window.localStorage.getItem("highscores")) || [];
+        var newScore = {
+            score: time,
+            name: name
+        };
+        highscores.push(newScore);
+        window.localStorage.setItem("highscores", JSON.stringify(highscores));
     }
 }
 
